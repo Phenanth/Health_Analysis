5e7275f8b0c4eeea4e8dd66df56b7f9c452d282c
phenanth <ewe_dom@yahoo.co.jp>
2019-05-22 12:46:51 +0800

    ADDED: script - assemble_tables.py

--- scripts/assemble_tables.py
+++ scripts/assemble_tables.py
@@ -13,11 +13,12 @@ items = [
 			'thyroid',
 			'tumour'
 		]
+num_mapping = {'10': 'a', '11': 'b', '12': 'c', '13': 'd', '14': 'e', '15': 'f', '16': 'g'}
+
 
 NUM_INDEX = 183
 
-
-sql_select_index = 'select scientificName from list_index'
+sql_select_index = 'select distinct scientificName from list_index'
 
 """
 整合report_[genre]表内所有数据
@@ -25,8 +26,9 @@ sql_select_index = 'select scientificName from list_index'
 """
 
 # 通用，根据SQL语句获取一个表中特定条件的数据
-def getDataFromDb(sql, data = None):
+def querySQL(sql, data = None):
 
+	temp = None
 	try:
 		with connection.cursor() as cursor:
 			try:
@@ -34,7 +36,7 @@ def getDataFromDb(sql, data = None):
 					cursor.execute(sql, data)
 				else:
 					cursor.execute(sql)
-				temp = cursor.fetchall()
+					temp = cursor.fetchall()
 
 			finally:
 				pass
@@ -50,8 +52,8 @@ def generateInsertSQL(data_indexs):
 
 	for i in range(NUM_INDEX - 1):
 		sql_insert = sql_insert + data_indexs[i]['scientificName'] + '`, `'
-
-	sql_insert = sql_insert + data_indexs[NUM_INDEX]['scientificName'] + '`, `numAbnormal_chemical`, `numAbnormal_blood`, `numAbnormal_general`, `numAbnormal_heart`, `numAbnormal_thyroid`, `numAbnormal_tumour`, `numAbnormal_total`) values ('
+		
+	sql_insert = sql_insert + data_indexs[NUM_INDEX - 1]['scientificName'] + '`, `numAbnormal_chemical`, `numAbnormal_blood`, `numAbnormal_general`, `numAbnormal_heart`, `numAbnormal_thyroid`, `numAbnormal_tumour`, `numAbnormal_total`) values ('
 
 	for i in range(NUM_INDEX + 8):
 		sql_insert = sql_insert + '%s, '
@@ -60,7 +62,7 @@ def generateInsertSQL(data_indexs):
 	return sql_insert
 
 
-# 取一个表数据
+# 取表数据
 def getSingleTable(item, reportId = None):
 
 	if not reportId:
@@ -68,49 +70,74 @@ def getSingleTable(item, reportId = None):
 	else:
 		sql_tmp = 'select * from report_' + item + ' where reportId=\"' + reportId + '\"'
 
-	data_tmp = getDataFromDb(sql_tmp)
+	data_tmp = querySQL(sql_tmp)
 
 	return data_tmp
 
 
-data_indexs = getDataFromDb(sql_select_index)
+data_indexs = querySQL(sql_select_index)
 sql_insert = generateInsertSQL(data_indexs)
 
 # 作为标准
 data_metrics = getSingleTable(item_first)
 
+
+count = 0
 # 对每个report进行的数据整合操作
 for data_metric in data_metrics:
 
-	count = 0
+	fail = 0
 	reportId = data_metric['reportId']
 	clientId = data_metric['clientId']
 	numAbnormals = [] # 用保存各表的异常值
 
 	data = (reportId, clientId)
+	data_temp = {}
 
 	# 根据reportId获得该Id其他表的数据
 	for item in items:
 
-		data_table = getSingleTable(item, reportId)[0]
-		length_data = len(data_table) - 3 # 统计该表有几个数据列，去掉没用的几列数据
-		count_tmp = count + length_data
-
-		print(data_table)
-		# 从index表中找匹配数据
-		while count < count_tmp:
-			if data_indexs[count]['scientificName']
-			data = data + (data_table[data_indexs[count]['scientificName']],)
-			count = count + 1
-
-		# 添加异常项，等待所有表项数据添加完毕后再进行添加
+		try:
+			data_table = getSingleTable(item, reportId)[0]
+		except:
+			fail = 1
+			break
+		data_temp = {**data_temp, **data_table}
 		numAbnormals.append(data_table['numAbnormal'])
 
-		count = count_tmp
+	if fail:
+		continue
 
-		# 目前有问题
-		print(data, numAbnormals)
+	# 合并单表数据
+	for i in range(len(data_indexs)):
+		data = data + (data_temp[data_indexs[i]['scientificName']],)
 
-	if reportId == '00000db4e5284f52a7742ae000553ec5':
-		break
+	# 合并abnormal数据并形成六位的标记
+	abnormal_total = ''
+	for i in range(len(items)):
+		data = data + (numAbnormals[i],)
+		try:
+			if numAbnormals[i] < 10:
+				abnormal_total = abnormal_total + str(numAbnormals[i])
+			else:
+				mapped_abnormal = num_mapping[str(numAbnormals[i])]
+				abnormal_total = abnormal_total + mapped_abnormal
+		except TypeError:
+			break
+			fail = 1
+	if fail:
+		continue
+	data = data + (abnormal_total,)
 
+	# print(sql_insert, data)
+
+	querySQL(sql_insert, data)
+	# connection.commit()
+
+	if (count + 1) % 300 == 0:
+		connection.commit()
+		print('Inserted:', count + 1)
+
+	count = count + 1
+
+connection.close()
